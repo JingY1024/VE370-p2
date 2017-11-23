25d76aad91e06aefe85817e1f899fef67a139036
JingY1024 <jingyuanfang1024@sjtu.edu.cn>
2017-11-23 18:00:52 +0800

    ssd

--- main.v
+++ main.v
@@ -205,6 +205,99 @@ module main(clk, reset,
 
 	MUX32 MUXresult(ResultWB, ReadDataWB, MemtoRegWB, WriteData);
 
+	// on board
+
+	divider_500 M1 (clock, Q1, reset, clock_1);
+
+	ring_counter M3 (clock_1, Q5, reset);
+
+	always @(*)
+		A <= Q5;
+
+	always @(A) begin
+		if (test == 5'b00000) Q2 <= PC_out;
+		else if (test == 5'b01000) Q2 <= rt0;
+		else if (test == 5'b01001) Q2 <= rt1;
+		else if (test == 5'b01010) Q2 <= rt2;
+		else if (test == 5'b01011) Q2 <= rt3;
+		else if (test == 5'b01100) Q2 <= rt4;
+		else if (test == 5'b01101) Q2 <= rt5;
+		else if (test == 5'b01110) Q2 <= rt6;
+		else if (test == 5'b01111) Q2 <= rt7;
+		else if (test == 5'b10000) Q2 <= rs0;
+		else if (test == 5'b10001) Q2 <= rs1;
+		else if (test == 5'b10010) Q2 <= rs2;
+		else if (test == 5'b10011) Q2 <= rs3;
+		else if (test == 5'b10100) Q2 <= rs4;
+		else if (test == 5'b10101) Q2 <= rs5;
+		else if (test == 5'b10110) Q2 <= rs6;
+		else if (test == 5'b10111) Q2 <= rs7;
+		else if (test == 5'b11000) Q2 <= rt8;
+		else if (test == 5'b11001) Q2 <= rt9;
+	end
+
+	always @ (A) begin
+		if (A == 4'b1110) Q <= Q2[3:0];
+		else if (A == 4'b1101) Q <= Q2[7:4];
+		else if (A == 4'b1011) Q <= Q2[11:8];
+		else if (A == 4'b0111) Q <= Q2[15:12];
+	end
+
+	always @ (Q)
+		  if (Q == 4'b0000) begin a<=0;b<=0;c<=0;d<=0;e<=0;f<=0;g<=1;  end
+	else if (Q == 4'b0001) begin a<=1;b<=0;c<=0;d<=1;e<=1;f<=1;g<=1;  end
+	else if (Q == 4'b0010) begin a<=0;b<=0;c<=1;d<=0;e<=0;f<=1;g<=0;  end
+	else if (Q == 4'b0011) begin a<=0;b<=0;c<=0;d<=0;e<=1;f<=1;g<=0;  end
+	else if (Q == 4'b0100) begin a<=1;b<=0;c<=0;d<=1;e<=1;f<=0;g<=0;  end
+	else if (Q == 4'b0101) begin a<=0;b<=1;c<=0;d<=0;e<=1;f<=0;g<=0;  end
+	else if (Q == 4'b0110) begin a<=0;b<=1;c<=0;d<=0;e<=0;f<=0;g<=0;  end
+	else if (Q == 4'b0111) begin a<=0;b<=0;c<=0;d<=1;e<=1;f<=1;g<=1;  end
+	else if (Q == 4'b1000) begin a<=0;b<=0;c<=0;d<=0;e<=0;f<=0;g<=0;  end
+	else if (Q == 4'b1001) begin a<=0;b<=0;c<=0;d<=0;e<=1;f<=0;g<=0;  end
+	else if (Q == 4'b1010) begin a<=0;b<=0;c<=0;d<=1;e<=0;f<=0;g<=0;  end
+	else if (Q == 4'b1011) begin a<=1;b<=1;c<=0;d<=0;e<=0;f<=0;g<=0;  end
+	else if (Q == 4'b1100) begin a<=0;b<=1;c<=1;d<=0;e<=0;f<=0;g<=1;  end
+	else if (Q == 4'b1101) begin a<=1;b<=0;c<=0;d<=0;e<=0;f<=1;g<=0;  end
+	else if (Q == 4'b1110) begin a<=0;b<=1;c<=1;d<=0;e<=0;f<=0;g<=0;  end
+	else begin a<=0;b<=0;c<=0;d<=0;e<=1;f<=1;g<=1; end
 
 
 endmodule
+
+module divider_500(clock,Q,reset,clock_out);
+	input clock, reset;
+	output [25:0]Q;
+	output clock_out;
+	reg [25:0]Q;
+	reg clock_out1;
+
+	initial begin Q <= 0; end
+
+	always @ (posedge reset or posedge clock)
+		begin
+			if (reset == 1'b1) Q <= 0;
+			else if (Q == 99999) begin clock_out1 <= 1; Q <= 0; end
+			else begin clock_out1 <= 0; Q <= Q + 1; end
+		end
+	assign clock_out = clock_out1;
+endmodule
+
+
+module ring_counter(clock, Q, reset);
+	input clock, reset;
+	output [3:0]Q;
+	reg [3:0]Q;
+
+	initial begin Q <= 4'b1110; end
+
+	always @ (posedge reset or posedge clock)
+		begin
+			if (reset == 1'b1) Q <= 4'b1110;
+			else if (Q == 4'b1110) Q <= 4'b0111;
+			else if (Q == 4'b1101) Q <= 4'b1110;
+			else if (Q == 4'b1011) Q <= 4'b1101;
+			else if (Q == 4'b0111) Q <= 4'b1011;
+			//else Q <= Q/2;
+		end
+endmodule
+
